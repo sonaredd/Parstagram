0d95ce25bfce418605e39d74967f3cf4861fffaa
Sonali Reddy <reddyson@msu.edu>
2021-03-21 23:45:24 -0400

    Part 2

--- Parstagram/SceneDelegate.swift
+++ Parstagram/SceneDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -16,7 +17,21 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        
+        
+        
+        
+        
+        if PFUser.current() != nil{
+            login()
+        }
         guard let _ = (scene as? UIWindowScene) else { return }
+    }
+    
+    func login(){
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        
+        window?.rootViewController = main.instantiateViewController(withIdentifier: "FeedNavigationController")
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
